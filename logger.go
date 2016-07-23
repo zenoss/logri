c040f797f39a03a13604c384a19637e15e3a1fad
Ian McCracken <ian.mccracken@gmail.com>
2016-07-23 13:12:45 -0500

    Add logger stuff

--- logger.go
+++ logger.go
@@ -1,23 +1,75 @@
 package logri
 
-import "github.com/Sirupsen/logrus"
+import (
+	"errors"
+	"strings"
+	"sync"
+
+	"github.com/Sirupsen/logrus"
+)
+
+const (
+	RootLoggerName              = ""
+	NilLevel       logrus.Level = 255
+)
+
+var (
+	ErrInvalidRootLevel = errors.New("The root logger must have a level")
+)
 
 type Logger struct {
+	mu       *sync.Mutex
 	name     string
 	parent   *Logger
-	level    logrus.Level
+	absLevel logrus.Level
 	children []*Logger
-	logrus.Logger
+	logger   *logrus.Logger
 }
 
-func (l *Logger) SetLevel(level logrus.Level) {
-	l.Level = level
+func (l *Logger) GetChild(name string) *Logger {
+	for _, part := range strings.Split(name, ".") {
+
+	}
+	return nil
+}
+
+func (l *Logger) SetLevel(level logrus.Level, propagate bool) error {
+	if level == l.absLevel {
+		return nil
+	}
+	if level == NilLevel && l.name == RootLoggerName {
+		return ErrInvalidRootLevel
+	}
+	l.absLevel = level
+	switch level {
+	case NilLevel:
+		l.logger.Level = l.parent.GetEffectiveLevel()
+	default:
+		l.logger.Level = level
+	}
+	if propagate {
+		l.propagate()
+	}
+	return nil
 }
 
 func (l *Logger) GetEffectiveLevel() logrus.Level {
-	return l.Level
+	return l.logger.Level
 }
 
 func (l *Logger) GetLevel() logrus.Level {
-	return l.Level
+	return l.absLevel
+}
+
+func (l *Logger) propagate() {
+	for _, child := range l.children {
+		go child.inheritLevel(l.logger.Level)
+	}
+}
+
+func (l *Logger) inheritLevel(parentLevel logrus.Level) {
+	if l.absLevel == NilLevel {
+		l.logger.Level = parentLevel
+		l.propagate()
+	}
 }
--- logger_test.go
+++ logger_test.go
@@ -2,11 +2,25 @@ package logri_test
 
 import (
 	"github.com/Sirupsen/logrus"
+	"github.com/Sirupsen/logrus/hooks/test"
 	. "github.com/iancmcc/logri"
 	. "gopkg.in/check.v1"
 )
 
 func (s *LogriSuite) TestAThing(c *C) {
+	logger := Logger{}
+	hook := test.NewLocal(logger)
+	logger.SetLevel(logrus.InfoLevel)
+	c.Assert(logger.Level, Equals, logrus.InfoLevel)
+}
+
+func (s *LogriSuite) TestUnsetLoggerLevel(c *C) {
+}
+
+func (s *LogriSuite) TestInheritLevelFromParent(c *C) {
+}
+
+func (s *LogriSuite) TestSetRootLoggerToNil(c *C) {
 	logger := Logger{}
 	logger.SetLevel(logrus.InfoLevel)
 	c.Assert(logger.GetLevel(), Equals, logrus.InfoLevel)
