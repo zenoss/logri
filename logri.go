c2cc95aae60c3a90fadcffeb2c99e84b13c3d461
Ian McCracken <ian.mccracken@gmail.com>
2016-08-12 10:33:27 -0500

    Add a bunch of comments

--- logri.go
+++ logri.go
@@ -3,6 +3,158 @@ package logri
 import "github.com/Sirupsen/logrus"
 
 var (
-	// RootLogger is the default created logger tree.
-	RootLogger logrus.FieldLogger = NewLoggerFromLogrus(logrus.New())
+	// RootLogger is the default logger tree.
+	RootLogger = NewLoggerFromLogrus(logrus.New())
+	// Verify we implement the right interface
+	_ logrus.FieldLogger = RootLogger
 )
+
+// Logger returns a logger from the default tree with the given name.
+func Logger(name string) *logrus.Logger {
+	return RootLogger.GetChild(name)
+}
+
+// WithField creates an entry from the root logger and adds a field to
+// it. If you want multiple fields, use `WithFields`.
+//
+// Note that it doesn't log until you call Debug, Print, Info, Warn, Fatal
+// or Panic on the Entry it returns.
+func WithField(key string, value interface{}) *logrus.Entry {
+	return RootLogger.WithField(key, value)
+}
+
+// WithFields creates an entry from the root logger and adds multiple
+// fields to it. This is simply a helper for `WithField`, invoking it
+// once for each field.
+//
+// Note that it doesn't log until you call Debug, Print, Info, Warn, Fatal
+// or Panic on the Entry it returns.
+func WithFields(fields logrus.Fields) *logrus.Entry {
+	return RootLogger.WithFields(fields)
+}
+
+// WithError creates an entry from the root logger and adds an error to it,
+// using the value defined in ErrorKey as key.
+func WithError(err error) *logrus.Entry {
+	return RootLogger.WithError(err)
+}
+
+// Debugf logs a message at level Debug on the standard logger.
+func Debugf(format string, args ...interface{}) {
+	RootLogger.Debugf(format, args...)
+}
+
+// Infof logs a message at level Info on the standard logger.
+func Infof(format string, args ...interface{}) {
+	RootLogger.Infof(format, args...)
+}
+
+// Printf logs a message at level Info on the standard logger.
+func Printf(format string, args ...interface{}) {
+	RootLogger.Printf(format, args...)
+}
+
+// Warnf logs a message at level Warn on the standard logger.
+func Warnf(format string, args ...interface{}) {
+	RootLogger.Warnf(format, args...)
+}
+
+// Warningf logs a message at level Warn on the standard logger.
+func Warningf(format string, args ...interface{}) {
+	RootLogger.Warningf(format, args...)
+}
+
+// Errorf logs a message at level Error on the standard logger.
+func Errorf(format string, args ...interface{}) {
+	RootLogger.Errorf(format, args...)
+}
+
+// Fatalf logs a message at level Fatal on the standard logger.
+func Fatalf(format string, args ...interface{}) {
+	RootLogger.Fatalf(format, args...)
+}
+
+// Panicf logs a message at level Panic on the standard logger.
+func Panicf(format string, args ...interface{}) {
+	RootLogger.Panicf(format, args...)
+}
+
+// Debug logs a message at level Debug on the standard logger.
+func Debug(args ...interface{}) {
+	RootLogger.Debug(args...)
+}
+
+// Info logs a message at level Info on the standard logger.
+func Info(args ...interface{}) {
+	RootLogger.Info(args...)
+}
+
+// Print logs a message at level Info on the standard logger.
+func Print(args ...interface{}) {
+	RootLogger.Print(args...)
+}
+
+// Warn logs a message at level Warn on the standard logger.
+func Warn(args ...interface{}) {
+	RootLogger.Warn(args...)
+}
+
+// Warning logs a message at level Warn on the standard logger.
+func Warning(args ...interface{}) {
+	RootLogger.Warning(args...)
+}
+
+// Error logs a message at level Error on the standard logger.
+func Error(args ...interface{}) {
+	RootLogger.Error(args...)
+}
+
+// Fatal logs a message at level Fatal on the standard logger.
+func Fatal(args ...interface{}) {
+	RootLogger.Fatal(args...)
+}
+
+// Panic logs a message at level Panic on the standard logger.
+func Panic(args ...interface{}) {
+	RootLogger.Panic(args...)
+}
+
+// Debugln logs a message at level Debug on the standard logger.
+func Debugln(args ...interface{}) {
+	RootLogger.Debugln(args...)
+}
+
+// Infoln logs a message at level Info on the standard logger.
+func Infoln(args ...interface{}) {
+	RootLogger.Infoln(args...)
+}
+
+// Println logs a message at level Info on the standard logger.
+func Println(args ...interface{}) {
+	RootLogger.Println(args...)
+}
+
+// Warnln logs a message at level Warn on the standard logger.
+func Warnln(args ...interface{}) {
+	RootLogger.Warnln(args...)
+}
+
+// Warningln logs a message at level Warn on the standard logger.
+func Warningln(args ...interface{}) {
+	RootLogger.Warningln(args...)
+}
+
+// Errorln logs a message at level Error on the standard logger.
+func Errorln(args ...interface{}) {
+	RootLogger.Errorln(args...)
+}
+
+// Fatalln logs a message at level Fatal on the standard logger.
+func Fatalln(args ...interface{}) {
+	RootLogger.Fatalln(args...)
+}
+
+// Panicln logs a message at level Panic on the standard logger.
+func Panicln(args ...interface{}) {
+	RootLogger.Panicln(args...)
+}
